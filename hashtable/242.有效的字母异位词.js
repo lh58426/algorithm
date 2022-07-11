03f16574775ecff68dc8b213e5bdb92013d27fe5
lh58426 <lh58426@163.com>
2022-07-11 10:39:38 +0800

    feat: update hash table topics

--- hashtable/242.有效的字母异位词.js
+++ hashtable/242.有效的字母异位词.js
@@ -10,8 +10,22 @@
  * @param {string} t
  * @return {boolean}
  */
-var isAnagram = function(s, t) {
+var isAnagram = function (s, t) {
+  if (s.length != t.length) return false;
+  const obj = {};
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] in obj) {
+      obj[s[i]]++;
+    } else {
+      obj[s[i]] = 1;
+    }
 
+    if (t[i] in obj) {
+      obj[t[i]]--;
+    } else {
+      obj[t[i]] = -1;
+    }
+  }
+  return Object.keys(obj).every((key) => obj[key] === 0);
 };
 // @lc code=end
-
